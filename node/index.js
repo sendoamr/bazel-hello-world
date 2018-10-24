44352322cda170d19d6ec65f4a17f58b79973eb3
Sendoa Moronta <sendoa.moronta@bbva.com>
2018-10-24 15:38:05 +0200

    Add node read json file and test

--- node/index.js
+++ node/index.js
@@ -1,1 +1,15 @@
-console.log('Node: Hello World!');
+var hello =function () {
+	console.log('Node: Hello World!');
+
+	var fs = require('fs');
+	 
+	fs.readFile('resources/hello.json', 'utf8', function(err, contents) {
+	    console.log(contents);
+	});
+}
+
+hello();
+
+module.exports = {
+  hello: hello
+};
---
+++ node/test.js
@@ -0,0 +1,6 @@
+var hello = require('./index');
+
+hello.hello();
+
+var assert = require('assert');
+assert(true);
