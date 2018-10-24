7a6f6d38ed43895b8e7effc70af102ce79b5cf23
Sendoa Moronta <sendoa.moronta@bbva.com>
2018-10-24 14:52:25 +0200

    Add java read json file and test

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,7 +1,40 @@
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Java: Hello World!");
-    }
+		InputStream helloStream = HelloWorld.class.getResourceAsStream("resources/hello.json"); 
+		System.out.println(getStringFromInputStream(helloStream));
 
+	}
+
+	private static String getStringFromInputStream(InputStream is) {
+		BufferedReader buffer = null;
+		StringBuilder content = new StringBuilder();
+
+		String line;
+		try {
+			buffer = new BufferedReader(new InputStreamReader(is));
+			while ((line = buffer.readLine()) != null) {
+				content.append(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (buffer != null) {
+				try {
+					buffer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return content.toString();
+	}
 }
---
+++ java/HelloWorldTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import org.junit.Assert;
+
+public class HelloWorldTest {
+
+    @Test
+	public void testMain()  {
+	    HelloWorld.main(null);
+	    Assert.assertTrue(true);
+	}
+}
