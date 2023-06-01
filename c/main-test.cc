1603c892e94486491b29c0b608a6cb7529c4d4f1
Moriya Tzubara <moriyat@wix.com>
2023-06-01 12:00:56 +0300

    clean

--- c/main-test.cc
+++
@@ -1,6 +0,0 @@
-#include "gtest/gtest.h"
-
-//TODO complete with lib
-TEST(HelloTest, GetGreet) {
-  EXPECT_EQ(0, 0);
-}
--- python/helloWorld.py
+++
@@ -1,6 +0,0 @@
-def hello_world():
-  print("Hello from a function") 
-  file = open("resources/hello.json", "r") 
-  print file.read()
-
-hello_world()
